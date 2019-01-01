c429d02ee924418f8381c0d0dca29240b28c435b
UkiAbe <unlimited.blade.works22@gmail.com>
2019-01-01 16:33:38 +0900

    add information of Players to seed

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,4 @@
 class Player < ApplicationRecord
-  validates :back_number, uniqueness: true
   has_many :attendances
   has_many :games, through: :attendances
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,210 @@ Player.create!(
     {
       back_number: 5,
       name: '稲葉 昌士'
-    }
+    },
+    {
+      back_number: 6,
+      name: '今中 健一'
+    },
+    {
+      back_number: 7,
+      name: '新井 達也'
+    },
+    {
+      back_number: 8,
+      name: '宮地 創'
+    },
+    {
+      back_number: 9,
+      name: '中里 浩'
+    },
+    {
+      back_number: 10,
+      name: '越 光司'
+    },
+    {
+      back_number: 11,
+      name: '山川 築久'
+    },
+    {
+      back_number: 12,
+      name: '青木 慧斗'
+    },
+    {
+      back_number: 13,
+      name: '東條 雅巳'
+    },
+    {
+      back_number: 14,
+      name: '伊藤 和'
+    },
+    {
+      back_number: 15,
+      name: '中町 友彦'
+    },
+    {
+      back_number: 16,
+      name: '宮武 智史'
+    },
+    {
+      back_number: 17,
+      name: '佐々木 雅嗣'
+    },
+    {
+      back_number: 18,
+      name: '田辺 当士哉'
+    },
+    {
+      back_number: 19,
+      name: '北川 千誉'
+    },
+    {
+      back_number: 20,
+      name: '花井 貴士'
+    },
+    {
+      back_number: 21,
+      name: '白川 栄祐'
+    },
+    {
+      back_number: 22,
+      name: '田代 圭司'
+    },
+    {
+      back_number: 23,
+      name: '家入 聖也'
+    },
+    {
+      back_number: 24,
+      name: '志田野 裕也'
+    },
+    {
+      back_number: 25,
+      name: '山田 哲也'
+    },
+    {
+      back_number: 26,
+      name: '高橋 亮仁'
+    },
+    {
+      back_number: 27,
+      name: '飯田 直'
+    },
+    {
+      back_number: 28,
+      name: '齋藤 頼一'
+    },
+    {
+      back_number: 29,
+      name: '山本 修一'
+    },
+    {
+      back_number: 30,
+      name: '栗田 一平'
+    },
+    {
+      back_number: 31,
+      name: '大井 悠介'
+    },
+    {
+      back_number: 32,
+      name: '北村 卓也'
+    },
+    {
+      back_number: 33,
+      name: '山上 真'
+    },
+    {
+      back_number: 34,
+      name: '島田 裕'
+    },
+    {
+      back_number: 35,
+      name: '舟橋 享'
+    },
+    {
+      back_number: 36,
+      name: '蔡 錦昌'
+    },
+    {
+      back_number: 37,
+      name: '村上 弘樹'
+    },
+    {
+      back_number: 39,
+      name: '石山 挙'
+    },
+    {
+      back_number: 40,
+      name: '横町 暢洋'
+    },
+    {
+      back_number: 43,
+      name: '亀尾 真吾'
+    },
+    {
+      back_number: 46,
+      name: '飯沼 諭'
+    },
+    {
+      back_number: 80,
+      name: '大久保 匠'
+    },
+    {
+      back_number: 81,
+      name: '江成 桂介'
+    },
+    {
+      back_number: 82,
+      name: '佐藤 幸三郎'
+    },
+    {
+      back_number: 83,
+      name: '伊藤 周平'
+    },
+    {
+      back_number: 84,
+      name: '水谷 陽平'
+    },
+    {
+      back_number: 85,
+      name: '阿部 修人'
+    },
+    {
+      back_number: 86,
+      name: '海老子 智'
+    },
+    {
+      back_number: 87,
+      name: '石垣 幸太'
+    },
+    {
+      back_number: 88,
+      name: '豊沢 修之'
+    },
+    {
+      back_number: 89,
+      name: '佐々木 一'
+    },
+    {
+      back_number: 90,
+      name: 'さと みか'
+    },
+    {
+      back_number: 91,
+      name: '古谷 まゆっぺ'
+    },
+    {
+      back_number: 92,
+      name: 'たえ ちゃん'
+    },
+    {
+      back_number: 93,
+      name: '太田 範之'
+    },
+    {
+      back_number: 94,
+      name: '玉木 剛'
+    },
   ]
 )
